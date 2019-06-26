ccc222a8b2ef9c42c1e2eb9b3aa2d8449353580a
masato-okuwaki <masato.okuwaki@di-v.co.jp>
2019-06-26 23:45:20 +0900

    third commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server '13.230.105.155', user: 'kazdeploy', roles: %w{app db web}
+server '3.114.1.240', user: 'ec2-user', roles: %w{app db web}
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
